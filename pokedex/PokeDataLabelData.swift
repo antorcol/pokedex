042fe3757216d1d4e9266b3462d0b61d78ef3751
Anthony Torrero Collins <atc@securegravity.com>
2016-03-07 15:16:59 -0800

    Modified colors of speaker and back arrow a bit.

--- pokedex/PokeDataLabelData.swift
+++ pokedex/PokeDataLabelData.swift
@@ -9,14 +9,15 @@
 import Foundation
 import UIKit
 
-class PokeDataLabelData: UILabel {
+class PokeDataLabelData: PaddingLabel {
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        let fontSize = self.font.pointSize;
+        let fontSize = CGFloat(14.0)// self.font.pointSize;
         self.font = UIFont(name: "HelveticaNeue", size: fontSize)
         self.textColor = UIColor(colorLiteralRed: 59/255, green: 59/255, blue: 59/255, alpha: 1.0)
         self.preferredMaxLayoutWidth = self.frame.width
+        
     }
 }
