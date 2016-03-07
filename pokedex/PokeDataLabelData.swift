93ef74189bf0d5883904dbd29416e97642349530
Anthony Torrero Collins <atc@securegravity.com>
2016-03-06 21:12:54 -0800

    Updated color scheme to Pikachu.

---
+++ pokedex/PokeDataLabelData.swift
@@ -0,0 +1,22 @@
+//
+//  PokeLabel.swift
+//  pokedex
+//
+//  Created by Anthony Torrero Collins on 3/6/16.
+//  Copyright © 2016 Anthony Torrero Collins. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PokeDataLabelData: UILabel {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let fontSize = self.font.pointSize;
+        self.font = UIFont(name: "HelveticaNeue", size: fontSize)
+        self.textColor = UIColor(colorLiteralRed: 59/255, green: 59/255, blue: 59/255, alpha: 1.0)
+        self.preferredMaxLayoutWidth = self.frame.width
+    }
+}
---
+++ pokedex/PokeDataLabelTitle.swift
@@ -0,0 +1,25 @@
+//
+//  PokeLabel.swift
+//  pokedex
+//
+//  Created by Anthony Torrero Collins on 3/6/16.
+//  Copyright © 2016 Anthony Torrero Collins. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PokeDataLabelTitle: PaddingLabel {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let fontSize = self.font.pointSize;
+        self.font = UIFont(name: "HelveticaNeue-Bold", size: fontSize)
+        self.textColor = UIColor(colorLiteralRed: 59/255, green: 59/255, blue: 59/255, alpha: 1.0)
+        self.backgroundColor = UIColor(colorLiteralRed: 248/255, green: 199/255, blue: 24/255, alpha: 0.75)
+        self.layer.cornerRadius = 5.0
+        self.clipsToBounds = true
+        self.preferredMaxLayoutWidth = self.frame.width
+    }
+}
